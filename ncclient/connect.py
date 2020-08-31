b957da535604f77f5b44c86645d9402ae3a1a50d
fredgan <ganshaolong@vip.qq.com>
2020-08-31 17:07:50 +0800

    add some testcase about asyncio

--- ncclient/connect.py
+++ ncclient/connect.py
@@ -5,7 +5,21 @@ if sys.version_info.major > 2:
     from ncclient import asyncio_manager
 
 def connect(*args, **kwds):
-    # use sync_mode by default
+    """
+    A common API for connecting to server and getting manager instance
+
+    Arguments:
+
+        *args - Position arguments into Manager.sync_connect or
+                AsyncioManager.async_connect
+
+        **kwds - Keyword arguments into Manager.sync_connect or
+                 AsyncioManager.async_connect
+
+    Returns:
+
+        A Manager or AsyncioManager instance depends on async_mode
+    """
     manager_params = kwds.get("manager_params", {"async_mode": False})
     if manager_params.get("async_mode") is True and sys.version_info.major > 2:
         return asyncio_manager.async_connect(*args, **kwds)
